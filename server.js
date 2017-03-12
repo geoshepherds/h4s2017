a6f512ecb8f4ab322cb4d8087c6ba88d333bc5ad
owain-stratton <owain@geoshepherds.com>
2017-03-12 06:15:29 +0100

    added shadow tile layer to leaflet

--- server.js
+++ server.js
@@ -1,19 +1,65 @@
 // import mapnik from 'mapnik';
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
 import compression from 'compression';
+import csv from 'fast-csv';
+import expressCSV from 'express-csv';
+import tilelive from 'tilelive';
+import tileliveFile from 'tilelive-file';
+
+tileliveFile.registerProtocols(tilelive);
+
 const app = express();
 
 app.use(compression());
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
+const MAP_FILE = path.join(__dirname, 'data/2016-12-21_winter_epsg3857/');
 const ENTRY_FILE = path.join(__dirname, 'index.html');
 const PORT = isDevelopment ? 3000 : process.env.PORT;
 
 app.use(express.static(__dirname));
-app.get('*', (req, res) => {
-   res.sendFile(path.resolve(ENTRY_FILE));
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
 });
+
+
+tilelive.load(`file://${MAP_FILE}`, (err, source) => {
+   if (err) throw err;
+
+   console.log(source);
+   app.get('/:z/:x/:y.png', (req, res) => {
+      console.log(req.params);
+      const { z, x, y } = req.params;
+
+      console.log(`Fetching %d %d %d ${z} ${x} ${y}`);
+      source.getTile(z, x, y, (err, tile, headers) => {
+         console.log(tile, headers);
+         if (err) {
+            res.status(404);
+            res.send(err.message);
+            console.log(err.message);
+         } else {
+            res.status(200);
+            res.set(headers);
+            res.send(tile);
+         }
+      });
+   });
+});
+
+// const sthlmBuildings = csv.fromPath('./resources/sthlm.csv')
+//    .on('data', (data) => {})
+//    .on('end', () => console.log('Done'));
+//
+// app.get('/buildings', (req, res) => {
+//    res.expressCSV(sthlmBuildings);
+//    req.end();
+// });
 
 // app.get('/:z/:x/:y', (req, res) => {
 //
